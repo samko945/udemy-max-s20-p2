30c9998a8b6586c1b239f81165aa82947bd3fea3
Samuel <samkoj@hotmail.co.uk>
2021-08-26 21:11:36 +0100

    Set up MainNavigation component

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+import { useParams, Route } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+
 export default function QuoteDetail() {
-	return <h1>Quote Detail Page</h1>;
+	const params = useParams();
+	return (
+		<React.Fragment>
+			<h1>Quote Detail Page</h1>
+			<p>{params.quoteId}</p>
+			<Route path="/quotes/:quoteId/comments">
+				<Comments />
+			</Route>
+		</React.Fragment>
+	);
 }
