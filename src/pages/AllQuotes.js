b4b65818f2ac9e5b136f0366d4c407f2ef234bac
Samuel <samkoj@hotmail.co.uk>
2021-08-26 20:33:01 +0100

    Set up basic routing to pages

---
+++ src/pages/AllQuotes.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AllQuotes() {
+	return <h1>All Quotes Page</h1>;
+}
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function QuoteDetail() {
+	return <h1>Quote Detail Page</h1>;
+}
