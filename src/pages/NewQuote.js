096bd7301d6428053eba4801d0a30ece6521bae5
Samuel <samkoj@hotmail.co.uk>
2021-08-27 20:20:13 +0100

    add programatic navigation at end of creation of new quote

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 export default function NewQuote() {
+	const history = useHistory();
 	function addQuoteHandler(quoteData) {
 		console.log(quoteData);
+		/**
+		 * push navigates the user to a new page allowing them to go back
+		 * replace navigates the user to a new page but going back will not take them to the replaced page
+		 */
+		history.push("/quotes");
 	}
 	return <QuoteForm onAddQuote={addQuoteHandler} />;
 }
